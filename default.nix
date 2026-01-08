2ad0b2926e3d8d7c357e058c56a2d0cccd75aedf
gquetel <quetel.gregor@gmail.com>
2026-01-08 11:21:41 +0100

    chores: reorganize code, add explanatory comments, add home-manager module

--- default.nix
+++ default.nix
@@ -1,15 +1,20 @@
 let
   inputs = import ./npins;
   nixpkgs = inputs.nixpkgs;
-  disko = import "${inputs.disko}/module.nix";
-  colmenaModules = import "${inputs.colmena}/src/nix/hive/options.nix";
 
+  disko-module = import "${inputs.disko}/module.nix";
+  colmena-modules = import "${inputs.colmena}/src/nix/hive/options.nix";
+  home-manager-module = import "${inputs.home-manager}/nixos/default.nix";
   # See: https://github.com/oddlama/nix-topology
   # We don't use flakes, we import the nix-topology overlay and module as follows:
   nix-topology-overlay = import "${inputs.nix-topology}/pkgs/default.nix";
   nix-topology-module = import "${inputs.nix-topology}/nixos/module.nix";
 
-  pkgs = import inputs.nixpkgs { };
+  pkgs = import inputs.nixpkgs {
+    # pkgs in topology requires rendering tools. We provide them through this
+    # overlay.
+    overlays = [ nix-topology-overlay ];
+  };
 in
 rec {
   colmena = {
@@ -34,18 +39,20 @@ rec {
           };
           modules = [
             value
-            disko
+            disko-module
             nix-topology-module
+            home-manager-module
             {
               nixpkgs.overlays = [
                 (final: prev: {
                   unstable = import inputs.unstable { config.allowUnfree = true; };
                 })
-                nix-topology-overlay
               ];
             }
           ];
-          extraModules = [ colmenaModules.deploymentOptions ];
+          # Colmena-specific options, not required for machine config
+          # but required to define deployment options.
+          extraModules = [ colmena-modules.deploymentOptions ];
         }
       )
       {
@@ -56,20 +63,14 @@ rec {
         vapula = import ./machines/vapula;
       };
 
-  # I am unsure, this is required way to do declare topology.
-  topologyPkgs = import inputs.nixpkgs {
-    system = "x86_64-linux";
-    overlays = [ nix-topology-overlay ];
-  };
-
+  # nix-topology expects a pkgs argument that already have a nix-topology overlay.
   topology = import inputs.nix-topology {
-    pkgs = topologyPkgs;
+    pkgs = pkgs;
     modules = [
       {
         nixosConfigurations = nixosConfigurations;
       }
       ./topology.nix
-
     ];
   };
 }
