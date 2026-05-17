756e2b255901e4621516c98ade61475c3e0eecb4
gquetel <quetel.gregor@gmail.com>
2026-05-17 16:36:02 +0200

    feat: add Marianne font to desktops

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -6,11 +6,24 @@
 }:
 
 let
-
+  marianne = pkgs.stdenv.mkDerivation {
+    name = "marianne-font";
+    src = pkgs.fetchzip {
+      url = "https://gitlab-forge.din.developpement-durable.gouv.fr/dreal-pdl/csd/propre.brochure/-/archive/master/propre.brochure-master.tar.gz?path=fonts/marianne/truetype";
+      hash = "sha256-XEyDeJ4iCjDyWBHLCFdsqb5Riel0qE9ZJWFg6BuOxBA=";
+      extension = "tar.gz";
+      stripRoot = false;
+    };
+    installPhase = ''
+      mkdir -p $out/share/fonts/truetype
+      cp propre.brochure-master-fonts-marianne-truetype/fonts/marianne/truetype/*.ttf $out/share/fonts/truetype/
+    '';
+  };
 in
 {
   config.fonts = {
     packages = with pkgs; [
+      marianne
       source-code-pro
       nerd-fonts.iosevka
       nerd-fonts.fira-code
