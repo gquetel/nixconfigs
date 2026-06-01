00012ce49d4bdaa3cd8e657cbd2c77d2c483d2ee
gquetel <quetel.gregor@gmail.com>
2026-06-01 14:56:12 +0200

    feat: replace inline Root CA definition by a file

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -36,22 +36,8 @@ in
 
   config = {
     security.pki.certificates = [
-      # custom step-ca root public certificate file.
-      # Required if i want my machines to trust certificates issued by my step-ca instance.
-
-      # Root CA generated manually
-      ''
-        -----BEGIN CERTIFICATE-----
-        MIIBdTCCARqgAwIBAgIRAPOAKlBcE/h/LuxFpQeINF4wCgYIKoZIzj0EAwIwGDEW
-        MBQGA1UEAxMNZ2FybXIgUm9vdCBDQTAeFw0yNTA3MTQxMDExMzRaFw0zNTA3MTIx
-        MDExMzRaMBgxFjAUBgNVBAMTDWdhcm1yIFJvb3QgQ0EwWTATBgcqhkjOPQIBBggq
-        hkjOPQMBBwNCAASKpNvqsVINura1WrF9bcj9hwTmKlbLZ2PA2Oc7rCROHCvrjAD5
-        0D2TFMi/5jHlLbKM5AoYu/4AMrg+EsxmgULGo0UwQzAOBgNVHQ8BAf8EBAMCAQYw
-        EgYDVR0TAQH/BAgwBgEB/wIBATAdBgNVHQ4EFgQUi6Hlc5zrjPuDVJkzcliP4OEI
-        hIgwCgYIKoZIzj0EAwIDSQAwRgIhAIquFboD0RZbpfRCmQur2qsw8Bk+d504IyNn
-        nA6kaXCXAiEAzJj3anHJZxCNi2UpSMfQKyACd/W7c56y+FcTOjvgPjM=
-        -----END CERTIFICATE-----
-      ''
+      # step-ca root CA. From: http://ca.mesh.gq/roots.pem
+      (builtins.readFile ../step-ca/roots.pem)
     ];
 
     # Enable firewall
