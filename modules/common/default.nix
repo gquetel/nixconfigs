1b7fac68c158634eebd459def32487b212b1e21b
gquetel <quetel.gregor@gmail.com>
2026-09-23 10:51:24 +0200

    feat: bump npins & update cachix cuda

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -59,11 +59,11 @@ in
         "flakes"
       ];
       substituters = [
-        "https://cuda-maintainers.cachix.org"
+        "https://cache.nixos-cuda.org"
         "https://cache.numtide.com"
       ];
       trusted-public-keys = [
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
         "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       ];
     };
